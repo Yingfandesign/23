304e21e489eda77d4dc0d40f06318d30ba7e33a8
Yingfan <1874375591@qq.com>
2022-09-29 02:44:06 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
+document.getElementById("add").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
